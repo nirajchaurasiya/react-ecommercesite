17825cac7efb96c26479c2cc130f9bcb6643d72e
Niraj kumar chaurasiya <116571542+nirajchaurasiya@users.noreply.github.com>
2023-06-09 09:19:37 +0545

    updates

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -27,6 +27,7 @@ export default function Dashboard() {
     const [numberOfUsers, setNumberOfUsers] = useState(0)
     const [loader, setLoader] = useState(true)
     const [selectedImages, setSelectedImages] = useState([]);
+    const [addClassToNav, setAddClassToNav] = useState('white')
     const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
 
     const title = useRef();
@@ -176,15 +177,15 @@ export default function Dashboard() {
                                 </div>
                                 <div className="p-4">
                                     <ul className="space-y-1">
-                                        <li onClick={() => { setAddProduct(false); setshowDashboard(true) }} >
-                                            <p className="flex items-center bg-indigo-200 rounded-xl font-bold text-sm text-indigo-900 py-3 px-4">
+                                        <li className='cursor-pointer select-none' onClick={() => { setAddProduct(false); setshowDashboard(true); }} >
+                                            <p className={`flex bg-${addClassToNav} hover:bg-indigo-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4`}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
                                                     <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z" />
                                                 </svg> Dashboard
                                             </p>
                                         </li>
-                                        <li onClick={() => { setAddProduct(true); setshowDashboard(false) }} >
-                                            <p className="flex bg-white hover:bg-indigo-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
+                                        <li className='cursor-pointer select-none' onClick={() => { setAddProduct(true); setshowDashboard(false); }} >
+                                            <p className={`flex bg-${addClassToNav} hover:bg-indigo-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4`}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
                                                     <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1z" />
                                                 </svg>Add Product
